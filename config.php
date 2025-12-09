d5377bae545d77cee9fed8ef54514eaf38614f96
WinterBelle <blanca.altamirano0318@gmail.com>
2025-12-09 15:24:41 -0500

    added .env

--- config.php
+++ config.php
@@ -34,4 +34,10 @@ if (!function_exists('csrf_verify')) {
         return hash_equals($_SESSION['csrf_token'], $_POST['csrf_token']);
     }
 }
+
+$env = parse_ini_file(__DIR__ . '/.env');
+foreach ($env as $key => $value) {
+    putenv("$key=$value");
+}
+
 ?>
